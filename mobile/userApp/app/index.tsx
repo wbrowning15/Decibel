0b5c61ede11d8903c11555dd29ca0315dd63c3a2
Wallace Browning <wbrownin@usc.edu>
2024-05-23 18:36:11 -0700

    small changes

--- mobile/userApp/app/index.tsx
+++ mobile/userApp/app/index.tsx
@@ -1,15 +1,18 @@
 import { Text, View } from "react-native";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Welcome to EventSphere!</Text>
+      </View>
+    </SafeAreaProvider>
   );
 }
